e0cdcd7c5d62be72bf5f34a3b5d8b8ef8d610f96
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-04-07 19:47:26 -0300

    Feature: Style of page Home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+
 import Header from './components/Header';
 
 function App() {
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,91 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Hello Felipao</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJ7G0xjJX-6QlCyT2g8Q05Ck091ms3mAvCv1LfHL9iOTran4dAqRW3Q0mIb4hp9w6BHfFnbzccglIh3Fw71gbpjrC_vHAXcV6-1rx5oYtPHLAIjqzuWDWn&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>R$149,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJ7G0xjJX-6QlCyT2g8Q05Ck091ms3mAvCv1LfHL9iOTran4dAqRW3Q0mIb4hp9w6BHfFnbzccglIh3Fw71gbpjrC_vHAXcV6-1rx5oYtPHLAIjqzuWDWn&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>R$149,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJ7G0xjJX-6QlCyT2g8Q05Ck091ms3mAvCv1LfHL9iOTran4dAqRW3Q0mIb4hp9w6BHfFnbzccglIh3Fw71gbpjrC_vHAXcV6-1rx5oYtPHLAIjqzuWDWn&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>R$149,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJ7G0xjJX-6QlCyT2g8Q05Ck091ms3mAvCv1LfHL9iOTran4dAqRW3Q0mIb4hp9w6BHfFnbzccglIh3Fw71gbpjrC_vHAXcV6-1rx5oYtPHLAIjqzuWDWn&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>R$149,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJ7G0xjJX-6QlCyT2g8Q05Ck091ms3mAvCv1LfHL9iOTran4dAqRW3Q0mIb4hp9w6BHfFnbzccglIh3Fw71gbpjrC_vHAXcV6-1rx5oYtPHLAIjqzuWDWn&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>R$149,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
