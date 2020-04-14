4a277548a0ddbdf064fd0d252a4805311df31377
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-04-14 18:05:30 -0300

    Chore: Adiconando produtos ao carrinho.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,7 +8,7 @@ import logo from '../../assets/images/logo.svg';
 
 import { Container, Cart } from './styles';
 
-export default function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -17,7 +18,7 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 itens</span>
+          <span>{cartSize} itens</span>
         </div>
 
         <MdShoppingBasket size={36} color="#FFF" />
@@ -25,3 +26,7 @@ export default function Header() {
     </Container>
   );
 }
+
+export default connect((state) => ({
+  cartSize: state.cart.length,
+}))(Header);
